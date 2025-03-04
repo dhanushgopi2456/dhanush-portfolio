a6fc338373fe64c5e5233776ca5303c47f3cb93e
dhanushgopi2456 <gopidhanush615@gmail.com>
2025-03-04 19:07:00 +0530

    first commit

---
+++ forms/send_mail.php
@@ -0,0 +1,21 @@
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
+
+    $to = "gopidhanush615@gmail.com";
+    $headers = "From: $email" . "\r\n" . "Reply-To: $email";
+    $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Failed to send message.";
+    }
+}
+?>
+
+
